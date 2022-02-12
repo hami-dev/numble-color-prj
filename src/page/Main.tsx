e40258ff46cdaece5a9502ff17e07a102079e5c1
hami-dev <rinrin.xml@gmail.com>
2022-02-13 01:37:38 +0900

    Add: 메인 화면 추가

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
-function Main() {
-    return <div>this is main</div>;
-}
+import SquareBox from 'components/SquareBox';
+import Score from 'components/Score';
+import Stage from 'components/Stage';
+import LeftTime from 'components/LeftTime';
+
+import useLeftTime from 'hooks/useLeftTime';
+import useStage from 'hooks/useStage';
+import useScore from 'hooks/useScore';
+
+const Main = () => {
+  const {
+    leftTime,
+    startCountdown,
+    resetCountdown,
+    stopCountdown,
+    minusCountdown,
+  } = useLeftTime();
+  const { stage, nextStage, resetStage } = useStage();
+  const { score, calcScore } = useScore();
+
+  const handleSuccessLevel = () => {
+    calcScore(stage, leftTime);
+    nextStage();
+    resetCountdown();
+  };
+
+  const handleFailLevel = () => {
+    minusCountdown();
+  };
+
+  useEffect(() => {
+    startCountdown();
+    return () => {
+      stopCountdown();
+    };
+  }, [startCountdown, stopCountdown]);
+
+  useEffect(() => {
+    if (leftTime <= 0) {
+      stopCountdown();
+      alert(`스테이지: ${stage}, 점수: ${score}`);
+      resetStage();
+      resetCountdown();
+    }
+  }, [leftTime]);
+
+  return (
+    <GameWrapper>
+      <ScoreWrapper>
+        <Stage stage={stage} />
+        <LeftTime
+          leftTime={leftTime}
+          startCountdown={startCountdown}
+          stopCountdown={stopCountdown}
+        />
+        <Score score={score} />
+      </ScoreWrapper>
+      <SquareBox
+        stage={stage}
+        handleSuccessLevel={handleSuccessLevel}
+        handleFailLevel={handleFailLevel}
+      />
+    </GameWrapper>
+  );
+};
 
 export default Main;
+
+const GameWrapper = styled.div`
+  padding: 50px;
+`;
+
+const ScoreWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+`;
